a537752585d2e499f03608e157ac137d0d02b171
charanGAMER <76534368+charanGAMER@users.noreply.github.com>
2024-06-30 20:04:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('userForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Here you can add code to handle form submission, e.g., sending data to a server or displaying a success message.
+    alert('Form submitted!');
+  });
+  
